88e6849e90a54e2e5f405894c63cf70ee166c130
B-Russell <43629901+B-Russell@users.noreply.github.com>
2019-01-25 19:48:38 -0500

    Update poller.py

--- WebProject1/poller.py
+++ WebProject1/poller.py
@@ -3,10 +3,9 @@ import time, app
 def main():
     while True:
         val = app.getTable()
-        print(val)
+        #print(val)
         for i in val:
-            app.update(i[1], i[0])
-            #Delete function           
-        time.sleep(15)
+            app.update(i[1], i[0])          
+        time.sleep(30)
         
 main()
