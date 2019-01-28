30fc5d790c46b5625c7a20d5752b3669fa46c0b4
B-Russell <43629901+B-Russell@users.noreply.github.com>
2019-01-27 22:47:07 -0500

    Update app.py

--- WebProject1/app.py
+++ WebProject1/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, json, flash, redirect, url_fo
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import time, smtplib, pymysql, subprocess
+import time, smtplib, pymysql
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2fdvab'
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 conn = pymysql.connect(host='localhost', port = 3307, user='root', passwd='pwd',db='userinfo') 
 cursor = conn.cursor()
@@ -22,10 +22,14 @@ def main():
         if form.validate():
             link=request.form['link']
             email=request.form['email']
-            flash('You will be notified on any changes in stock or price')
-            insert(link, email)
+            if link[0:26] != 'https://shop.havenshop.com':
+                flash('Not registered. Please enter a Haven address')
+            else:
+                flash('You will be notified on any changes in stock or price')
+                insert(link, email)
     return render_template('WebPage1.html', form=form)
 
+#Scrapes data on stock and price
 def scrapeData(link, email):
     #data = 'https://shop.havenshop.com/products/undercover-ucv3805-t-shirt-white'
     early_page = urlopen(link)
@@ -41,6 +45,7 @@ def scrapeData(link, email):
     tup = (stock, new_price)
     return tup
 
+#Inserts new row into table in SQL
 def insert(link, email):
     tup = scrapeData(link, email)
     stock = tup[0]
@@ -53,12 +58,14 @@ def insert(link, email):
     except:
         conn.rollback()
 
+#Gets all rows in SQL table
 def getTable():
     qu = "Select * from haven"
     cursor.execute(qu)
     pls = cursor.fetchall()
     return pls
 
+#Checks for updates in stock or price and if true, emails user and updates SQL database
 def update(link, email):
     tup = scrapeData(link, email)
     new_stock = tup[0]
@@ -68,27 +75,29 @@ def update(link, email):
     WHERE email = %s AND page = %s AND (stock != %s OR price != %s)"
     val = (new_stock, new_price.text, email, link, new_stock, new_price.text)
     try:
-        cursor.execute(query, val)
+        if cursor.execute(query, val) == 1: #Email only if row is updated
+            mail(link, email, new_stock, new_price.text)
         conn.commit()
-        mail(link, email, new_stock, new_price)
     except:
         conn.rollback()
     if new_stock == '0000':
         return False
     return True
 
+#Email notification function
 def mail(link, email, stock, price):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("stock.webapp.notification@gmail.com", "pwd")
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login("stock.webapp.notification@gmail.com", "pwd")
 
-    message = "Details for product" + link + "Price: " + price + "Small stock: " + stock[0] \
-    + "Medium stock: " + stock[1] + "Large stock: " + stock[2] + "Extra large stock: " + stock[3] \
-    + "This message was sent from... If you wish to stop receiving messages ..."
-    server.sendmail("stock.webapp.notification@gmail.com", email, message)
-    server.quit()
-
+        message = "\nDetails for product " + link + "\nPrice: " + price + "\nSmall stock: " + stock[0] \
+        + "\nMedium stock: " + stock[1] + "\nLarge stock: " + stock[2] + "\nExtra large stock: " + stock[3] \
+        + "\nThis message was sent from the haven web notification app."
+        server.sendmail("stock.webapp.notification@gmail.com", email, message)
+        server.quit()
+    except:
+        print('Error')
 
 if __name__ == '__main__':
     app.run('localhost')
-
